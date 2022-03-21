6d477f8888ee53aecc61200d27d903913fe4265d
febo <febo@kent.ac.uk>
2022-03-21 01:08:19 +0000

    Checking if config file is found and readable

--- src/config/errors.rs
+++ src/config/errors.rs
@@ -2,11 +2,21 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ConfigError {
-    #[error("Error parsing the config file: {0}")]
+    #[error("cound not parse the config file ({0})")]
     ParseError(String),
 
-    #[error("Error opening the config file: {0}")]
-    FileOpenError(String),
-    #[error("Invalid cluster: {0}")]
+    #[error("missing configuration file '{0}'")]
+    MissingFileError(String),
+
+    #[error("the configuration file path is invalid ('{0}' is a drectory)")]
+    InvalidPathError(String),
+
+    #[error("cound not open config file '{0}'")]
+    PermissionError(String),
+
+    #[error("invalid cluster '{0}'")]
     InvalidCluster(String),
+
+    #[error("invalid upload method '{0}'")]
+    InvalidUploadMethod(String),
 }
