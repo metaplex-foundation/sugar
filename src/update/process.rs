dfc601c3bf47b8365f070100d5c97ec42476c81f
Sarah Strange <strange1120@gmail.com>
2022-06-18 18:28:20 -0400

    chore(update): remove println

--- src/update/process.rs
+++ src/update/process.rs
@@ -176,7 +176,6 @@ fn create_candy_machine_data(
     candy_machine: CandyMachineData,
 ) -> Result<CandyMachineData> {
     info!("{:?}", config.go_live_date);
-    println!("{:#?}", candy_machine);
     let go_live_date = Some(go_live_date_as_timestamp(&config.go_live_date)?);
 
     let end_settings = config.end_settings.as_ref().map(|s| s.into_candy_format());
