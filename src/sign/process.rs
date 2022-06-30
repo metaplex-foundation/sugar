95a01ae3d131a845181d08b7c7cf62620f6a4e13
Sarah Strange <strange1120@gmail.com>
2022-06-30 16:43:59 -0400

    feat(sign): sign one finished

--- src/sign/process.rs
+++ src/sign/process.rs
@@ -10,6 +10,7 @@ pub use anchor_client::{
     Client, Program,
 };
 use console::style;
+use retry::{delay::Exponential, retry};
 use solana_account_decoder::UiAccountEncoding;
 use solana_client::{
     rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
@@ -19,10 +20,12 @@ use std::{
     io::{stdin, stdout, Write},
     rc::Rc,
     str::FromStr,
+    sync::Arc,
 };
 
 use mpl_candy_machine::accounts as nft_accounts;
 use mpl_candy_machine::instruction as nft_instruction;
+use mpl_token_metadata::{instruction::sign_metadata, state::Metadata, ID as METAPLEX_PROGRAM_ID};
 
 use crate::setup::{setup_client, sugar_setup};
 use crate::utils::*;
@@ -48,7 +51,7 @@ pub fn process_sign(args: SignArgs) -> Result<()> {
     let pb = spinner_with_style();
     pb.set_message("Connecting...");
 
-    let (program, payer) = setup_sign(args.keypair, args.rpc_url)?;
+    let (program, payer, creator) = setup_sign(args.keypair, args.rpc_url)?;
 
     let candy_machine_id = match args.candy_machine {
         Some(candy_machine_id) => candy_machine_id,
@@ -58,12 +61,14 @@ pub fn process_sign(args: SignArgs) -> Result<()> {
         }
     };
 
+    let candy_machine = Pubkey::from_str(&candy_machine_id)?;
+
     pb.finish_with_message("Connected");
 
     println!(
-        "\n{} {}{} funds",
+        "\n{} {}{}",
         style("[2/2]").bold().dim(),
-        WITHDRAW_EMOJI,
+        SIGNING_EMOJI,
         if args.mint.is_some() {
             "Signing one NFT"
         } else {
@@ -78,8 +83,8 @@ pub fn process_sign(args: SignArgs) -> Result<()> {
         let account_pubkey = Pubkey::from_str(&mint_id)?;
         let metadata_pubkey = get_metadata_pda(&account_pubkey, &program)?;
 
-        let result = match sign(program, candy_machine_id, payer) {
-            Ok(signature) => format!("{} {}", style("Signature:").bold(), signature),
+        let result = match sign(program, &creator, metadata_pubkey.0) {
+            Ok(signature) => format!("{} {:?}", style("Signature:").bold(), signature),
             Err(err) => {
                 pb.abandon_with_message(format!("{}", style("Signing failed ").red().bold()));
                 error!("{:?}", err);
@@ -88,42 +93,46 @@ pub fn process_sign(args: SignArgs) -> Result<()> {
         };
 
         pb.finish_with_message(result);
-    } else {
-        let pb = progress_bar_with_style(number);
-
-        for _i in 0..number {
-            if let Err(err) = mint(
-                Arc::clone(&client),
-                candy_pubkey,
-                Arc::clone(&candy_machine_state),
-                Arc::clone(&collection_pda_info),
-            ) {
-                pb.abandon_with_message(format!("{}", style("Mint failed ").red().bold()));
-                error!("{:?}", err);
-                return Err(err);
-            }
-
-            pb.inc(1);
-        }
-
-        pb.finish();
     }
+    // else {
+    //     let pb = progress_bar_with_style(number);
+
+    //     for _i in 0..number {
+    //         if let Err(err) = mint(
+    //             Arc::clone(&client),
+    //             candy_pubkey,
+    //             Arc::clone(&candy_machine_state),
+    //             Arc::clone(&collection_pda_info),
+    //         ) {
+    //             pb.abandon_with_message(format!("{}", style("Mint failed ").red().bold()));
+    //             error!("{:?}", err);
+    //             return Err(err);
+    //         }
+
+    //         pb.inc(1);
+    //     }
+
+    //     pb.finish();
+    // }
 
     Ok(())
 }
 
-fn setup_sign(keypair: Option<String>, rpc_url: Option<String>) -> Result<(Program, Pubkey)> {
+fn setup_sign(
+    keypair: Option<String>,
+    rpc_url: Option<String>,
+) -> Result<(Program, Pubkey, Keypair)> {
     let sugar_config = sugar_setup(keypair, rpc_url)?;
     let client = setup_client(&sugar_config)?;
     let program = client.program(CANDY_MACHINE_ID);
     let payer = program.payer();
 
-    Ok((program, payer))
+    Ok((program, payer, sugar_config.keypair))
 }
 
-fn sign(program: Rc<Program>, candy_machine: Pubkey, payer: Pubkey) -> Result<()> {
-    let recent_blockhash = client.get_latest_blockhash()?;
-    let ix = sign_metadata(METAPLEX_PROGRAM_ID, metadata_pubkey, creator.pubkey());
+fn sign(program: Program, creator: &Keypair, metadata: Pubkey) -> Result<Signature> {
+    let recent_blockhash = program.rpc().get_latest_blockhash()?;
+    let ix = sign_metadata(METAPLEX_PROGRAM_ID, metadata, creator.pubkey());
     let tx = Transaction::new_signed_with_payer(
         &[ix],
         Some(&creator.pubkey()),
@@ -134,7 +143,7 @@ fn sign(program: Rc<Program>, candy_machine: Pubkey, payer: Pubkey) -> Result<()
     // Send tx with retries.
     let res = retry(
         Exponential::from_millis_with_factor(250, 2.0).take(3),
-        || client.send_and_confirm_transaction(&tx),
+        || program.rpc().send_and_confirm_transaction(&tx),
     );
     let sig = res?;
 
