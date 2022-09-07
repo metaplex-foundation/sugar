c87f4c0f55006617b72d3197236757c00270f45a
DeepGreenAI <william@deepgreenai.com>
2022-09-07 16:11:11 -0400

    verify that filename arrays contain exactly one element and remove unused UploadDataArgs struct definition

--- src/upload/assets.rs
+++ src/upload/assets.rs
@@ -15,14 +15,6 @@ use serde_json;
 
 use crate::{common::*, validate::format::Metadata};
 
-pub struct UploadDataArgs<'a> {
-    pub bundlr_client: Arc<Bundlr<SolanaSigner>>,
-    pub assets_dir: &'a Path,
-    pub extension_glob: &'a str,
-    pub tags: Vec<Tag>,
-    pub data_type: DataType,
-}
-
 #[derive(Debug, Clone)]
 pub enum DataType {
     Image,
@@ -194,7 +186,7 @@ pub fn get_asset_pairs(assets_dir: &str) -> Result<HashMap<isize, AssetPair>> {
             .filter(|p| img_regex.is_match(p))
             .collect::<Vec<String>>();
 
-        let img_filename = if img_filenames.is_empty() {
+        let img_filename = if img_filenames.len() != 1 {
             let error = if is_collection_index {
                 anyhow!("Couldn't find the collection image filename.")
             } else {
@@ -242,7 +234,7 @@ pub fn get_asset_pairs(assets_dir: &str) -> Result<HashMap<isize, AssetPair>> {
             .expect("Failed to convert image path from unicode.")
             .to_string();
 
-        let animation_filename = if !animation_filenames.is_empty() {
+        let animation_filename = if animation_filenames.len() == 1 {
             let animation_filepath = Path::new(assets_dir)
                 .join(&animation_filenames[0])
                 .to_str()
