1d891aa0bb1f3fdb36ff13b1ebe930fe0aed315f
Zain <76736607+zayn-kahn@users.noreply.github.com>
2022-09-28 13:48:33 +0500

    works without cluster
    Already made a PR to the metaplex docs.
    https://github.com/metaplex-foundation/docs/pull/348
    
    e.g:
    
    works: https://www.solaneyes.com/address/6886xPKD5V8T927fJnktPCd7JBREsfEBnjCGu1LSm5y5
    
    does not work: https://www.solaneyes.com/address/6886xPKD5V8T927fJnktPCd7JBREsfEBnjCGu1LSm5y5devnet
    
    you need to change the network from the UI of solaneyes.

--- src/verify/process.rs
+++ src/verify/process.rs
@@ -222,9 +222,8 @@ pub fn process_verify(args: VerifyArgs) -> Result<()> {
         println!("\nVerification successful. You're good to go!");
     } else {
         println!(
-            "\nVerification successful. You're good to go!\n\nSee your candy machine at:\n  -> https://www.solaneyes.com/address/{}?cluster={}",
+            "\nVerification successful. You're good to go!\n\nSee your candy machine at:\n  -> https://www.solaneyes.com/address/{}",
             cache.program.candy_machine,
-            cluster
         );
     }
     Ok(())
