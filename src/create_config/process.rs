4afd2c79176519c2fcbb0fdb894ce407ae3a8225
febo <febo@metaplex.com>
2022-10-05 22:06:13 +0100

    Allow empty default values

--- src/create_config/process.rs
+++ src/create_config/process.rs
@@ -630,12 +630,13 @@ pub fn process_create_config(args: CreateConfigArgs) -> Result<()> {
 
         let directory = Input::with_theme(&theme)
             .with_prompt("What is the directory to upload to? Leave blank to store files at the bucket root dir.")
-            .default(String::from(""))
+            .allow_empty(true)
             .interact()
             .unwrap();
 
         let domain: String = Input::with_theme(&theme)
             .with_prompt("Do you have a custom domain? Leave blank to use AWS default domain.")
+            .allow_empty(true)
             .interact()
             .unwrap();
 
