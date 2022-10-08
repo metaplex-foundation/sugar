aded6afe952b985ffa0c8aac07cf0b9597d2e99d
Samuel J Vanderwaal <samuel.vanderwaal@gmail.com>
2022-10-08 13:03:35 -0800

    remove unused dep

--- src/freeze/mod.rs
+++ src/freeze/mod.rs
@@ -11,7 +11,7 @@ use mpl_candy_machine::{
 use serde::{Deserialize, Serialize, Serializer};
 use solana_client::{rpc_client::RpcClient, rpc_request::RpcRequest};
 use solana_program::{instruction::AccountMeta, program_pack::Pack};
-use solana_transaction_crawler::crawler::Crawler;
+// use solana_transaction_crawler::crawler::Crawler;
 use spl_associated_token_account::{create_associated_token_account, get_associated_token_address};
 use spl_token::state::Account as SplAccount;
 use tokio::sync::Semaphore;
